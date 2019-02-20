9300ff9e76a7c3904255e6cec441bf9a935744c3
Paulo Tarcisio <33143476+tarcisiopqdt@users.noreply.github.com>
2019-02-20 15:08:15 -0300

    Infinity-Scroll-Bug-Fixed

--- client-server/app/public/js/modules/infinity-scroll.js
+++ client-server/app/public/js/modules/infinity-scroll.js
@@ -1,20 +1,18 @@
-import initApiRequest from './noticias-api.js';
-export default function(){
-    
-    window.addEventListener("scroll", function(){
-        const viewport = document.documentElement.clientHeight;
-        const container = document.body.offsetHeight ;
-        let scrollHeight = container - viewport;
-        
-        if(scrollHeight == this.scrollY){
-               console.log("agora!!!!")
-            
-               setTimeout(function(){ initApiRequest(8); }, 500);
-            
-         }
-    })
+import initApiRequest from "./noticias-api.js";
+export default function() {
+  window.addEventListener("scroll", function() {
+    const viewport = document.documentElement.clientHeight;
+    const container = document.body.offsetHeight;
+    let scrollHeight = container - viewport;
+    let scroll = this.scrollY;
 
- 
+    if (
+      scrollHeight == Math.ceil(scroll) ||
+      scrollHeight == Math.floor(scroll)
+    ) {
+      initApiRequest(8);
+    }
+  });
 }
 
    
