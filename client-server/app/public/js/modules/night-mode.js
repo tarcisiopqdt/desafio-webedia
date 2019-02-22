3073e94ee7bce71b601aee063d4a89c96c98b130
Paulo Tarcisio <paulo_tarcisio93@hotmail.com>
2019-02-22 10:50:14 -0300

    Update night-mode.js

--- client-server/app/public/js/modules/night-mode.js
+++ client-server/app/public/js/modules/night-mode.js
@@ -1,18 +1,33 @@
 export default function initNightMode(){
+    const nightModeStorage = localStorage.getItem('gmtNightMode');
     const checkbox = document.querySelector("#checkbox-night-mode");
     const class_night_mode = document.querySelectorAll(".js-night-mode");
 
+    if (nightModeStorage) {
+        class_night_mode.forEach(item=>{
+            item.classList.add("night-mode");
+            checkbox.checked = true;
+        })
+    }
+
     checkbox.addEventListener('click', function() {
 
-        if(this.checked) {
-            class_night_mode.forEach(item=>{
-                item.classList.add("night-mode");
-            })
-        } else {
-            class_night_mode.forEach(item=>{
-                item.classList.remove("night-mode");
-            })
-        }
+        class_night_mode.forEach(item=>{
+            item.classList.toggle("night-mode");
 
+            if ( item.classList.contains('night-mode') ) {
+                localStorage.setItem('gmtNightMode', true);
+                return;
+            }
+
+            localStorage.removeItem('gmtNightMode');
+    
+
+
+
+
+        })
+
+        
     });
 }
